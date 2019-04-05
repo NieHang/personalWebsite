077a26b49c329aa3f7a2ec030860b2b67db156f3
NieHang <1076883711@qq.com>
2019-04-05 15:01:44 +0800

    删除SecondHome组件，不需要了

--- src/containers/SecondHome/index.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-import './style.css'
-
-export default class SecondHome extends Component {
-	render() {
-		return <div className={this.props.status ? 'shome__true' : 'shome'} />;
-	}
-}
