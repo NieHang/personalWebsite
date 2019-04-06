26262cc3547bef45625cf61893b9de2256e6c06a
NieHang <1076883711@qq.com>
2019-04-06 12:47:26 +0800

    箭头位置整改

--- src/containers/Control/index.js
+++ src/containers/Control/index.js
@@ -5,11 +5,21 @@ export default class Control extends Component {
 	render() {
 		const { status } = this.props;
 		return (
-			<div
-				className={status ? 'control__true' : 'control'}
-			>
-				<span className='control__rhombus' onClick={this.handleClick.bind(this, status)}></span>
-				<img className={status ? 'control__cat' : 'control__cat__Y'} src="https://i.loli.net/2019/04/05/5ca6f2b2db40d.jpg" alt="caihongCat"/>
+			<div className={status ? 'control__true' : 'control'}>
+				<div className="control__change">
+					<span
+						className="control__rhombus"
+						onClick={this.handleClick.bind(this, status)}
+					/>
+					{
+						status ? <span className="control__rightRow" /> : <span className="control__leftRow" />
+					}
+				</div>
+				<img
+					className={status ? 'control__cat' : 'control__cat__Y'}
+					src="https://i.loli.net/2019/04/05/5ca6f2b2db40d.jpg"
+					alt="caihongCat"
+				/>
 			</div>
 		);
 	}
