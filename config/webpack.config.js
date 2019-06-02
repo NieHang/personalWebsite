822303dee9e4e25a0dfc1c0818122d8a6776d245
NieHang <1076883711@qq.com>
2019-06-02 18:04:00 +0800

    移动端适配需要的插件和插件配置

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -24,6 +24,12 @@ const ModuleNotFoundPlugin = require('react-dev-utils/ModuleNotFoundPlugin');
 const ForkTsCheckerWebpackPlugin = require('react-dev-utils/ForkTsCheckerWebpackPlugin');
 const typescriptFormatter = require('react-dev-utils/typescriptFormatter');
 
+const postcssAspectRatioMini = require('postcss-aspect-ratio-mini');
+const postcssPxToViewport = require('postcss-px-to-viewport');
+const postcssWriteSvg = require('postcss-write-svg');
+const postcssViewportUnits = require('postcss-viewport-units');
+const cssnano = require('cssnano');
+
 // Source maps are resource heavy and can cause out of memory issue for large source files.
 const shouldUseSourceMap = process.env.GENERATE_SOURCEMAP !== 'false';
 // Some apps do not need the benefits of saving a web request, so not inlining the chunk
@@ -96,6 +102,25 @@ module.exports = function(webpackEnv) {
               },
               stage: 3,
             }),
+            postcssAspectRatioMini({}),
+            postcssPxToViewport({ 
+              viewportWidth: 750, // (Number) The width of the viewport. 
+              viewportHeight: 1334, // (Number) The height of the viewport. 
+              unitPrecision: 3, // (Number) The decimal numbers to allow the REM units to grow to. 
+              viewportUnit: 'vw', // (String) Expected units. 
+              selectorBlackList: ['.ignore', '.hairlines'], // (Array) The selectors to ignore and leave as px. 
+              minPixelValue: 1, // (Number) Set the minimum pixel value to replace. 
+              mediaQuery: false // (Boolean) Allow px to be converted in media queries. 
+            }),
+            postcssWriteSvg({
+              utf8: false
+            }),
+            postcssViewportUnits({}),
+            cssnano({
+              preset: "default", 
+              autoprefixer: false, 
+              "postcss-zindex": false 
+            })
           ],
           sourceMap: isEnvProduction && shouldUseSourceMap,
         },
