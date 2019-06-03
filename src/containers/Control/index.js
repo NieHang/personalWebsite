262d9214222923f6ab5e8e85ed6caa6203284e28
NieHang <1076883711@qq.com>
2019-06-03 16:03:25 +0800

    优化Control Css样式

--- src/containers/Control/index.js
+++ src/containers/Control/index.js
@@ -6,14 +6,23 @@ export default class Control extends Component {
 		const { status } = this.props;
 		return (
 			<div className={status ? 'control__true' : 'control'}>
-				<div className="control__change">
+				<div className="control__change" onClick={this.handleClick.bind(this, status)}>
 					<span
 						className="control__rhombus"
-						onClick={this.handleClick.bind(this, status)}
 					/>
-					{
-						status ? <span className="control__rightRow" /> : <span className="control__leftRow" />
-					}
+					{status ? (
+						<img
+							src="https://i.loli.net/2019/06/03/5cf4cc2b8bea661407.png"
+							className="control__rightRow"
+							alt='右箭头'
+						/>
+					) : (
+						<img
+							src="https://i.loli.net/2019/06/03/5cf4cc2b8bea661407.png"
+							className="control__leftRow"
+							alt='左箭头'
+						/>
+					)}
 				</div>
 				<img
 					className={status ? 'control__cat' : 'control__cat__Y'}
