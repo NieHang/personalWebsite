8712c675c845f2007cd5c14c80b183f74cc77f22
NieHang <1076883711@qq.com>
2019-06-05 21:26:07 +0800

    图片懒加载问题解决

---
+++ src/containers/Study/components/ItemWeb/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import LazyLoad from 'react-lazyload';
+
+export default function ItemWeb(props) {
+	return (
+		<div className="item__web">
+			<a href={props.address} target="block">
+				{/* 如果在设有overflow属性的容器(Study)中懒加载图片，需要在LazyLoad组件设置overflow属性值为true */}
+				<LazyLoad
+					scrollContainer={document.querySelector('#study')}
+					overflow={true}
+					height={60}
+				>
+					<img src={props.imgUrl} alt='' />
+				</LazyLoad>
+				<p>{props.name}</p>
+			</a>
+		</div>
+	);
+}
