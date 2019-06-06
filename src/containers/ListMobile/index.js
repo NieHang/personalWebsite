b69f4c47cb2794c5990fbd95f582ab7fcf52f783
NieHang <1076883711@qq.com>
2019-06-06 15:41:57 +0800

    修改ListMobile组件样式

--- src/containers/ListMobile/index.js
+++ src/containers/ListMobile/index.js
@@ -7,23 +7,45 @@ export default class ListMobile extends Component {
 	render() {
 		const { showList, bgColor } = this.props;
 		return (
-			<div className={showList ? (bgColor ? "list__show bgColor" : 'list__show') : 'list__hide'}>
+			<div
+				className={
+					showList
+						? bgColor
+							? 'list__show bgColor'
+							: 'list__show'
+						: 'list__hide'
+				}
+			>
 				<ul className="list__links">
-					<Link to="/" onClick={this.handleClick.bind(this, showList)}>Home</Link>
-					<Link to="/project" onClick={this.handleClick.bind(this, showList)}>Project</Link>
-					<Link to="/study" onClick={this.handleClick.bind(this, showList)}>Study</Link>
-					<Link to="/barrage" onClick={this.handleClick.bind(this, showList)}>Barrage</Link>
+					<Link to="/" onClick={this.handleClick.bind(this, showList)}>
+						Home
+					</Link>
+					<Link to="/project" onClick={this.handleClick.bind(this, showList)}>
+						Project
+					</Link>
+					<Link to="/study" onClick={this.handleClick.bind(this, showList)}>
+						Study
+					</Link>
+					<Link to="/barrage" onClick={this.handleClick.bind(this, showList)}>
+						Barrage
+					</Link>
 					<div className="list__contacts">
 						<img
 							src="https://i.loli.net/2019/04/05/5ca72974ccc87.png"
 							alt="微信"
 						/>
-            <img src="https://i.loli.net/2019/04/05/5ca72b578ddea.png" alt="wechatQRcode"/>
+						<img
+							src="https://i.loli.net/2019/06/06/5cf8846e5051b66958.png"
+							alt="wechatQRcode"
+						/>
 						<img
 							src="https://i.loli.net/2019/04/05/5ca72974ce809.png"
 							alt="qq"
 						/>
-            <img src="https://i.loli.net/2019/04/05/5ca72dba94020.jpg" alt="qqQRcode"/>
+						<img
+							src="https://i.loli.net/2019/06/06/5cf8845a79edf58433.jpg"
+							alt="qqQRcode"
+						/>
 					</div>
 				</ul>
 			</div>
