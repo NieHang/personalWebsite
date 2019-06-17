d350ab21b65940da0bbc00ebf05f7b810019651b
NieHang <1076883711@qq.com>
2019-06-17 13:53:04 +0800

    使用hooks改造弹幕组件

--- src/containers/Barrage/index.js
+++ src/containers/Barrage/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Draggable from 'react-draggable';
 // eslint-disable-next-line
 import axios from 'axios';
@@ -6,81 +6,63 @@ import './css/style.css';
 
 import BarrageItem from './components/BarrageItem';
 
-export default class Barrage extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			barrages: [
-				<BarrageItem key='hero' index={'hero'} value={'这个世界需要英雄！'} />
-			],
-			value: '',
-			key: 0
-		};
-	}
-	render() {
-		return (
-			<div className="barrage">
-				<div className="barrage__list">
-					{this.state.barrages.map(item => item)}
-				</div>
-				<div className="barrage__form">
-					<Draggable
-						handle=".handle"
-						defaultPosition={{ x: 0, y: 0 }}
-						position={null}
-						grid={[25, 25]}
-						scale={1}
-					>
-						<img
-							src="https://i.loli.net/2019/04/03/5ca4a63c11e58.png"
-							alt="头像"
-							className="handle"
-						/>
-					</Draggable>
-					<p>欢迎来到小肥橘猫妙妙屋</p>
-					<p>发个弹幕吧</p>
-					<p>
-						<input
-							type="text"
-							placeholder="biubiubiu~"
-							onChange={this.handleChange.bind(this)}
-						/>
-						<img
-							src="https://i.loli.net/2019/04/06/5ca8607559fa4.png"
-							alt="biu"
-							onClick={this.handleClick.bind(this)}
-						/>
-					</p>
-				</div>
-			</div>
-		);
-	}
-
-	componentDidMount() {
+export default function Barrag() {
+	const [barrages, setBarrages] = useState([<BarrageItem key='hero' index={'hero'} value={'这个世界需要英雄！'} />])
+	const [value, setValue] = useState('')
+	let   [key, setKey] = useState(0)
+	
+	useEffect(() => {
 		// axios.get('').then(res => {
 		// 	console.log(res)
 		// })
+	})
+
+	function handleChange(e) {
+		setValue(e.target.value)
 	}
 
-	handleChange(e) {
-		this.setState({
-			value: e.target.value
-		});
+	function handleClick() {
+		setKey(key += 1)
+		setBarrages([
+			...barrages,
+			<BarrageItem key={key} value={value} />
+		])
 	}
-
-	handleClick() {
-		this.setState(
-			(state, props) => ({
-				key: (state.key += 1)
-			}),
-			() => {
-				this.setState({
-					barrages: [
-						...this.state.barrages,
-						<BarrageItem key={this.state.key} value={this.state.value} />
-					]
-				});
-			}
-		);
-	}
+	
+	return (
+		<div className="barrage">
+			<div className="barrage__list">
+				{barrages.map(item => item)}
+			</div>
+			<div className="barrage__form">
+				<Draggable
+					handle=".handle"
+					defaultPosition={{ x: 0, y: 0 }}
+					position={null}
+					grid={[25, 25]}
+					scale={1}
+				>
+					<img
+						src="https://i.loli.net/2019/04/03/5ca4a63c11e58.png"
+						alt="头像"
+						className="handle"
+					/>
+				</Draggable>
+				<p>欢迎来到小肥橘猫妙妙屋</p>
+				<p>发个弹幕吧</p>
+				<p>
+					<input
+						type="text"
+						placeholder="biubiubiu~"
+						onChange={handleChange}
+					/>
+					<img
+						src="https://i.loli.net/2019/04/06/5ca8607559fa4.png"
+						alt="biu"
+						onClick={handleClick}
+					/>
+				</p>
+			</div>
+		</div>
+	);
 }
