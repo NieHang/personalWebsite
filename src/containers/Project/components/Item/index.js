f37e5fa82e799320396d53adb54aef02374f0c33
NieHang <1076883711@qq.com>
2019-06-17 14:50:54 +0800

    将Project的子组件改为无状态组件

--- src/containers/Project/components/Item/index.js
+++ src/containers/Project/components/Item/index.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './style.css';
 
-export default class Item extends Component {
-	render() {
-		const { item } = this.props;
-		return (
-			<div className="item">
-				<a href={item.link} target="_block">
-					<img src={item.imgUrl} alt={item.name} className="item__img" />
-				</a>
-        <p className='item__name'>
-          <span></span>
-          <span>{item.name}</span>
-        </p>
-			</div>
-		);
-	}
+export default function Item(props) {
+	const { item } = props;
+	return (
+		<div className="item">
+			<a href={item.link} target="_block">
+				<img src={item.imgUrl} alt={item.name} className="item__img" />
+			</a>
+			<p className="item__name">
+				<span />
+				<span>{item.name}</span>
+			</p>
+		</div>
+	);
 }
