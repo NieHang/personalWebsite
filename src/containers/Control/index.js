ba921017814c1bee55d55df802dfe018cafd4673
NieHang <1076883711@qq.com>
2019-06-17 14:36:59 +0800

    使用hooks改造Control组件

--- src/containers/Control/index.js
+++ src/containers/Control/index.js
@@ -1,41 +1,38 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './css/style.css';
 
-export default class Control extends Component {
-	render() {
-		const { status } = this.props;
-		return (
-			<div className={status ? 'control__true' : 'control'}>
-				<div className="control__change" onClick={this.handleClick.bind(this, status)}>
-					<span
-						className="control__rhombus"
-					/>
-					{status ? (
-						<img
-							src="https://i.loli.net/2019/06/03/5cf4cc2b8bea661407.png"
-							className="control__rightRow"
-							alt='右箭头'
-						/>
-					) : (
-						<img
-							src="https://i.loli.net/2019/06/03/5cf4cc2b8bea661407.png"
-							className="control__leftRow"
-							alt='左箭头'
-						/>
-					)}
-				</div>
-				<img
-					className={status ? 'control__cat' : 'control__cat__Y'}
-					src="https://i.loli.net/2019/06/06/5cf879562670f81334.jpg"
-					alt="caihongCat"
-				/>
-			</div>
-		);
-	}
+export default function Control(props) {
+	const { status, changeBgColor } = props;
 
-	handleClick(status) {
-		if (this.props.changeBgColor) {
-			this.props.changeBgColor(status);
+	function handleClick(status) {
+		if (changeBgColor) {
+			changeBgColor(status);
 		}
 	}
+
+	return (
+		<div className={status ? 'control__true' : 'control'}>
+			<div className="control__change" onClick={() => handleClick(status)}>
+				<span className="control__rhombus" />
+				{status ? (
+					<img
+						src="https://i.loli.net/2019/06/03/5cf4cc2b8bea661407.png"
+						className="control__rightRow"
+						alt="右箭头"
+					/>
+				) : (
+					<img
+						src="https://i.loli.net/2019/06/03/5cf4cc2b8bea661407.png"
+						className="control__leftRow"
+						alt="左箭头"
+					/>
+				)}
+			</div>
+			<img
+				className={status ? 'control__cat' : 'control__cat__Y'}
+				src="https://i.loli.net/2019/06/06/5cf879562670f81334.jpg"
+				alt="caihongCat"
+			/>
+		</div>
+	);
 }
