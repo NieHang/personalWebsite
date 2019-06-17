2b8fb032e73e130d96987a98f5cfb997cb49262f
NieHang <1076883711@qq.com>
2019-06-17 15:22:27 +0800

    使用hooks改造Study组件

--- src/containers/Study/index.js
+++ src/containers/Study/index.js
@@ -1,84 +1,73 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import ItemWeb from './components/ItemWeb';
 import { throttle } from 'lodash';
 
 import './css/style.css';
 
-export default class Study extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			data: [],
-			showBack: false
-		};
-		this.handleScroll = this.handleScroll.bind(this);
-		this.changeBackStatusThrottled = throttle(this.changeBackStatus.bind(this), 200);
-	}
+export default function Study() {
+	const [data, setData] = useState([]);
+	const [showBack, setShowBack] = useState(false);
 
-	render() {
-		const { data, showBack } = this.state;
-		return (
-			<section
-				className="study"
-				id="study"
-				onScroll={this.handleScroll}
-			>
-				<div
-					className={showBack ? 'back' : ''}
-					onClick={this.handleBack.bind(this)}
-				/>
-				{data.map(value => (
-					<article className="study__item" key={value.id}>
-						<p className="item__title">{value.title}</p>
-						<div className="item__wrap">
-							{value.websites.map(item => (
-								<ItemWeb
-									key={item.id}
-									name={item.name}
-									imgUrl={item.imgUrl}
-									address={item.address}
-								/>
-							))}
-						</div>
-					</article>
-				))}
-			</section>
-		);
-	}
+	/**
+	 * 给滚动事件进行节流
+	 */
+	const changeBackStatusThrottled = throttle(changeBackStatus, 500);
 
-	async componentDidMount() {
+	const fetchData = async () => {
 		const {
 			data: { data }
 		} = await axios.get('./websites.json');
-		this.setState({
-			data
-		});
-	}
 
-	componentWillUnmount() {
-		this.changeBackStatusThrottled.cancel();
-	}
+		setData(data);
+	};
 
-	handleScroll(e) {
+	useEffect(
+		() => {
+			fetchData();
+		},
+		() => {
+			changeBackStatusThrottled.cancel();
+		}
+	);
+
+	function handleScroll(e) {
 		const currentTarget = e.currentTarget;
-		this.changeBackStatusThrottled(currentTarget)
+		changeBackStatusThrottled(currentTarget);
 	}
 
-	changeBackStatus(currentTarget) {
+	/** 改变回退按钮的状态 */ 
+	function changeBackStatus(currentTarget) {
 		if (typeof currentTarget === 'object') {
-			this.setState({
-				showBack: currentTarget.scrollTop > 300 ? true : false
-			});
+			setShowBack(currentTarget.scrollTop > 300 ? true : false);
 		}
 	}
 
-	handleBack() {
-		if (this.state.showBack) {
+	function handleBack() {
+		if (showBack) {
 			document.getElementById('study').scrollTop = 0;
-			this.setState({
-				showBack: false
-			})
+			setShowBack(false);
 		}
 	}
+
+	return (
+		<section className="study" id="study" onScroll={handleScroll}>
+			<div className={showBack ? 'back' : ''} onClick={handleBack} />
+			{data.map(value => (
+				<article className="study__item" key={value.id}>
+					<p className="item__title">{value.title}</p>
+					<div className="item__wrap">
+						{value.websites.map(item => (
+							<ItemWeb
+								key={item.id}
+								name={item.name}
+								imgUrl={item.imgUrl}
+								address={item.address}
+							/>
+						))}
+					</div>
+				</article>
+			))}
+		</section>
+	);
 }
